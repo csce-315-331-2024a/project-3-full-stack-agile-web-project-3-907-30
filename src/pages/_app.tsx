0fd2439b890b136fdd754f0dec77ce826f25bae8
ysadamt <adam.teo321@gmail.com>
2024-03-25 09:21:40 -0500

    Finish UI components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,47 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import App, { AppContext, AppInitialProps } from "next/app";
+import { AuthProvider, getGoogleAccountFromSession } from "../context/authContext";
+import Layout from "./layout";
+import { ElementType } from "react";
+import { GoogleAccount } from "@/lib/types";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+interface MyAppProps {
+  Component: ElementType;
+  pageProps: any;
+  account: GoogleAccount;
 }
+
+/**
+ * The MyApp component
+ * 
+ * @component
+ * @param {MyAppProps} props The props of the component
+ * @returns {JSX.Element} The JSX element
+ */
+export default function MyApp({ Component, pageProps, account }: MyAppProps) {
+  return (
+    <AuthProvider ssrAccount={account}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+  );
+}
+
+/**
+ * Get the initial props
+ * 
+ * @param {AppContext} appContext The app context
+ * @returns {Promise<AppInitialProps>} The app initial props
+ */
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  if (appContext.router.isSsr === undefined) {
+    const appProps: AppInitialProps = await App.getInitialProps(appContext);
+    const ctx: any = appContext.ctx;
+    const account = await getGoogleAccountFromSession(ctx);
+    return { ...appProps, account: account };
+  } else {
+    const appProps: AppInitialProps = await App.getInitialProps(appContext);
+    return { ...appProps };
+  }
+};
