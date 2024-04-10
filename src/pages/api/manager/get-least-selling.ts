74907819eb76883bd57b0263e6c940eff9684859
Aaron <aaronmathews@tamu.edu>
2024-04-10 00:32:54 -0500

    Finished backend function to get 10 least selling items

--- src/pages/api/manager/get-least-selling.ts
+++ src/pages/api/manager/get-least-selling.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MenuItem } from "../../../lib/types";
 import db from "../../../lib/db";
 
 /**
@@ -16,40 +17,35 @@ export default async function handler(
   }
 
 
-    try {
-    // First, Get IDs for each of the menu items
-    const IdSelectStatement = await db.prepare(
-        "SELECT item_name FROM menu_items ORDER BY item_id ASC"
-    );
-  
-    const IdSelectStatementResult = await IdSelectStatement.execute();
-  
-    await IdSelectStatement.close();
-  
-    const rows = IdSelectStatementResult.rows!;
-    const menuItemsOrderedById: string[] = rows.map((row) => row[0]);
 
-    const selectStatement = await db.prepare(
-      `SELECT menu_items.item_id, menu_items.item_name, menu_items.item_price, COUNT(orders_menu.item_id)
-      FROM menu_items
-      INNER JOIN orders_menu ON menu_items.item_id = orders_menu.item_id
-      INNER JOIN orders ON orders_menu.order_id = orders.order_id  
-      GROUP BY menu_items.item_id
-      ORDER BY COUNT(menu_items.item_id) LIMIT 10`
-    );
+const selectStatement = await db.prepare(
+    `SELECT menu_items.item_id, menu_items.item_name, menu_items.item_price, COUNT(orders_menu.item_id)
+    FROM menu_items
+    INNER JOIN orders_menu ON menu_items.item_id = orders_menu.item_id
+    INNER JOIN orders ON orders_menu.order_id = orders.order_id  
+    GROUP BY menu_items.item_id
+    ORDER BY COUNT(menu_items.item_id) LIMIT 10`
+);
 
 
-    const selectStatementResult = await selectStatement.execute();
+const selectStatementResult = await selectStatement.execute();
 
-    const rows2 = selectStatementResult.rows!;
+const rows = selectStatementResult.rows!;
 
-    await selectStatement.close();
+await selectStatement.close();
 
-    if (rows.length === 0) {
-        res.status(404).json({ error: "Errors" });
-      } else {
-        
-        
-        res.status(200).json();
-      }
+if (rows.length === 0) {
+    res.status(404).json({ error: "Errors" });
+}
+else {
+    // convert the rows to an array of Employee objects
+const items: MenuItem[] = rows.map((row) => ({
+    id: row[0],
+    name: row[1],
+    price: row[2],
+    times_ordered: row[3],
+    }));
+    
+    res.status(200).json(items);
+    }
 }
