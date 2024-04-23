71a4e2024f416980d1d70f6546b5c937540f0d14
chris <avilabros12@gmail.com>
2024-04-23 15:18:58 -0500

    Added ingredient picking, and data picker to add a seasonal item

--- src/components/manager/management/item-sale-gui.tsx
+++ src/components/manager/management/item-sale-gui.tsx
@@ -7,6 +7,8 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../..
 import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from '../../ui/form';
 import { updateMenuItemPrice } from '@/lib/utils';
 
+
+
 const FormSchema = z.object({
   item_name: z.string(),
   item_price: z.string()
@@ -27,7 +29,8 @@ const FormSchema = z.object({
    */
 const ItemSaleGUI = () => {
 
-  
+
+ 
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
