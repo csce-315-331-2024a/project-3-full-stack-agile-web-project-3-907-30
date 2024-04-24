c4219cb8b3c95e9098726594f78e52a137c7b87d
ysadamt <adam.teo321@gmail.com>
2024-04-24 09:41:36 -0500

    testing fixes

--- tests/employee_view.spec.ts
+++ tests/employee_view.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from 'playwright-test-coverage';
+import 'dotenv/config';
 
 test('Trying to use employee pages without signing in', async ({ page }) => {
-    await page.goto('https://project-3-full-stack-agile-web-project-3-907-30.vercel.app/');
-    await page.goto('https://project-3-full-stack-agile-web-project-3-907-30.vercel.app/employee/order');
+    await page.goto(`${process.env.TEST_URL}/`);
+    await page.goto(`${process.env.TEST_URL}/employee/order`);
     await expect(page.locator('[id="__next"]')).toContainText('You are unauthorized to view this page.');
     await expect(page.getByRole('link')).toContainText('Customer Order View');
     await expect(page.getByRole('button')).toContainText('Sign-in with Google');
