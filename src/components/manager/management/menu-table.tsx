b28119f6d1b4a2faa6605ef169af7314dd4a3b07
margongora <margongora0803@gmail.com>
2024-05-04 11:39:28 -0500

    Added filtering

--- src/components/manager/management/menu-table.tsx
+++ src/components/manager/management/menu-table.tsx
@@ -4,7 +4,9 @@ import {
     getCoreRowModel,
     useReactTable,
     SortingState,
-    getSortedRowModel
+    getSortedRowModel,
+    ColumnFiltersState,
+    getFilteredRowModel
 } from "@tanstack/react-table"
 
 import {
@@ -20,6 +22,7 @@ import { DetailedMenuItem } from "@/lib/types";
 import { Button } from "@/components/ui/button";
 import { deleteMenuItem } from "@/lib/utils";
 import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -32,6 +35,7 @@ export function MenuTable<TData extends DetailedMenuItem | undefined, TValue>({
 }: DataTableProps<TData, TValue>) {
     const [tableData, setTableData] = useState<TData[]>([]);
     const [sorting, setSorting] = useState<SortingState>([]);
+    const [filters, setFilters] = useState<ColumnFiltersState>([]);
 
     const table = useReactTable({
         data: tableData,
@@ -40,8 +44,11 @@ export function MenuTable<TData extends DetailedMenuItem | undefined, TValue>({
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
         state: {
-            sorting
-        }
+            sorting,
+            columnFilters: filters,
+        },
+        onColumnFiltersChange: setFilters,
+        getFilteredRowModel: getFilteredRowModel(),
     })
 
     useEffect(() => {
@@ -49,57 +56,69 @@ export function MenuTable<TData extends DetailedMenuItem | undefined, TValue>({
     }, [data])
 
     return (
-        <Table className="overflow-hidden">
-            <TableHeader>
-                {table.getHeaderGroups().map((headerGroup) => (
-                    <TableRow key={headerGroup.id}>
-                        {headerGroup.headers.map((header) => {
-                            return (
-                                <TableHead key={header.id}>
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )}
-                                </TableHead>
-                            )
-                        })}
-                    </TableRow>
-                ))}
-            </TableHeader>
-            <TableBody>
-                {table.getRowModel().rows?.length ? (
-                    table.getRowModel().rows.map((row, index) => {
-                        return (<TableRow
-                            key={row.id}
-                            data-state={row.getIsSelected() && "selected"}
+        <>
+            <div className="flex items-center py-2">
+                <Input
+                    placeholder="Filter items by name..."
+                    value={(table.getColumn("item_name")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn("item_name")?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+            </div>
+            <Table className="overflow-hidden">
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row, index) => {
+                            return (<TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
 
-                        >
-                            {row.getVisibleCells().map((cell) => (
-                                <TableCell key={cell.id}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                                <TableCell className="flex gap-2">
+                                    <MenuManagementForm menuItem={data[index]} editMode={true} />
+                                    <Button onClick={() => {
+                                        deleteMenuItem(data[index]!.item_id);
+                                        data = data.filter((value) => value !== data[index]);
+                                        console.log(data);
+                                        setTableData(data);
+                                    }}>X</Button>
                                 </TableCell>
-                            ))}
-                            <TableCell className="flex gap-2">
-                                <MenuManagementForm menuItem={data[index]} editMode={true} />
-                                <Button onClick={() => {
-                                    deleteMenuItem(data[index]!.item_id);
-                                    data = data.filter((value) => value !== data[index]);
-                                    console.log(data);
-                                    setTableData(data);
-                                }}>X</Button>
+                            </TableRow>)
+                        })
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
                             </TableCell>
-                        </TableRow>)
-                    })
-                ) : (
-                    <TableRow>
-                        <TableCell colSpan={columns.length} className="h-24 text-center">
-                            No results.
-                        </TableCell>
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </>
     )
 }
